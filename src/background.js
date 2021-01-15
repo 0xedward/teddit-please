aaefd2f0964ef9246af96730339e2275860fa576
Edward <14011954+0xedward@users.noreply.github.com>
2021-01-15 07:27:13 -0500

    Make code cleaner by setting default values when getting user's teddit URL

--- src/background.js
+++ src/background.js
@@ -1,38 +1,17 @@
 'use strict';
 
-let tedditURL = '';
-
 try {
-  chrome.storage.sync.onChanged.addListener(function(changes) {
-    if (changes.tedditURL !== undefined) {
-      tedditURL = changes.tedditURL.newValue;
-      console.log(`teddit URL changed to ${tedditURL}`);
-    }
+  chrome.storage.sync.get({'tedditURL': 'https://teddit.net'}, function(items) {
+    // https:// stackoverflow.com/questions/16928912/url-forwarding-using-chrome-webrequest-after-response-is-received#answer-16932715
+    chrome.webRequest.onBeforeRequest.addListener(function(details) {
+      const redditURL = new URL(details.url);
+      const redirectURL = `${items.tedditURL}${redditURL.pathname}${redditURL.search}`;
+      console.log(`Redirecting ${redditURL} to ${redirectURL}`);
+      return {redirectUrl: redirectURL};
+    }, {
+      urls: ['*://*.reddit.com/*'],
+    }, ['blocking']);
   });
 } catch (err) {
   console.log(err);
 }
-
-if (tedditURL === '') {
-  try {
-    chrome.storage.sync.get(['tedditURL'], function(items) {
-      if (items.tedditURL !== undefined) {
-        tedditURL = items.tedditURL;
-      } else {
-        tedditURL = 'https://teddit.net';
-      }
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-// https:// stackoverflow.com/questions/16928912/url-forwarding-using-chrome-webrequest-after-response-is-received#answer-16932715
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  const redditURL = new URL(details.url);
-  const redirectURL = `${tedditURL}${redditURL.pathname}${redditURL.search}`;
-  console.log(`Redirecting ${redditURL} to ${redirectURL}`);
-  return {redirectUrl: redirectURL};
-}, {
-  urls: ['*://*.reddit.com/*'],
-}, ['blocking']);
